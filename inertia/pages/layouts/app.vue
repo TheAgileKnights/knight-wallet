03868b44f959630bf76a266ad745a1cecb6f2ee5
Sam Kauffman <109776716+Samueljkauff@users.noreply.github.com>
2025-09-16 12:49:24 -0400

    SD-2: Install Tailwind (#1)
    * tailwind installed
    
    * updated inertia layout file
    
    * co piolet
    
    * SD-6: Tailwind Colors
    
    commit 258f55436f61e4ce2775d233ffa609020290b58a
    Merge: b05980b 427c26f
    Author: martipops <melonnemann@gmail.com>
    Date:   Sun Sep 14 18:19:10 2025 -0400
    
        Merge remote-tracking branch 'origin' into SD-6
    
    commit b05980b4bca0e1add31ea36dcdc2cb39f86ba98f
    Author: martipops <melonnemann@gmail.com>
    Date:   Sun Sep 14 18:16:23 2025 -0400
    
        Safeproof
    
    commit c4d43b684d1d911c35ca7bae2680b548406e2b3b
    Author: martipops <melonnemann@gmail.com>
    Date:   Sun Sep 14 18:15:03 2025 -0400
    
        Change webpage title
    
    commit 35e8f11f1388194215dc1bd680c599ac68625907
    Author: martipops <melonnemann@gmail.com>
    Date:   Sun Sep 14 17:59:16 2025 -0400
    
        Create tailwind color schemes and update edge view
    
    commit 1ee8b2a941e4b545403641881ec41eb43b909499
    Author: samueljkauff <samuelkauff@gmail.com>
    Date:   Thu Sep 11 22:21:09 2025 -0400
    
        tailwind installed
    
    ---------
    
    Co-authored-by: samueljkauff <samuelkauff@gmail.com>
    Co-authored-by: martipops <melonnemann@gmail.com>

---
+++ inertia/pages/layouts/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-screen h-screen">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    components: {
+    },
+    data() {
+      return {}
+    }
+  }
+</script>
