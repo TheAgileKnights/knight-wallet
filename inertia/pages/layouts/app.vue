80b2b775b2b877ea261e31b06c179a5405329b81
samueljkauff <samuelkauff@gmail.com>
2025-09-17 00:54:29 -0400

    style fix

--- inertia/pages/layouts/app.vue
+++ inertia/pages/layouts/app.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen">
     <slot />
   </div>
-  <footer class="bg-primary text-white py-4 w-full">
+  <footer class="bg-primary text-text-contrast py-4 w-full">
     <div class="container mx-auto text-center">
       <p class="text-text-contrast">&copy; 2025 Knight Wallet. Built with love by the Agile Knights</p>
       <p class="text-text-contrast-secondary">"Track Together, Settle Smarter"</p>
