9b5e013cf85cfde308251a131f2966be0d8746a2
samueljkauff <samuelkauff@gmail.com>
2025-09-16 22:44:13 -0400

    Better Icons

--- inertia/pages/home.vue
+++ inertia/pages/home.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="mx-[12.5%]">
-  <p class="font-bold text-7xl w-full text-center mt-8">Features</p>
-  <p class="text-center text-2xl my-4">Discover the powerful features that make our app stand out.</p>
+  <p class="font-bold text-6xl w-full text-center mt-8">Features</p>
+  <p class="text-center text-3xl my-4">Discover the powerful features that make our app stand out.</p>
   <div class="grid grid-cols-3">
     <div v-for="(card, index) in featureCards" :key="index" class="card flex flex-col justify-center items-center p-10 m-4 hover:scale-105 transition-transform duration-300">
-        <Icon class="mx-1 text-9xl m-16 text-accent" :icon="card.icon"/>
-        <p class="font-bold text-4xl m-auto">{{ card.name }}</p>
-        <p class="text-lg my-2">{{ card.description }}</p>
+        <Icon class="text-9xl mx-16 my-12 text-accent text-center" :icon="card.icon"/>
+        <p class="font-bold text-5xl m-auto text-center">{{ card.name }}</p>
+        <p class="text-2xl my-2 text-center">{{ card.description }}</p>
     </div>
   </div>
   </div>
@@ -21,10 +21,10 @@ export default {
       featureCards: [
         { name: 'Group Creation', description: 'Create friend groups and manage members on the go.', icon: 'mdi:people' },
         { name: 'Organization', description: 'Add and categorize expenses with ease.', icon: 'eos-icons:organization' },
-        { name: 'Multiple Groups', description: 'Split expenses among selectable group members.', icon: 'codicon:organization' },
-        { name: 'Multi-Currency', description: 'Multi-currency support for international use.', icon: 'mdi:currency-usd' },
-        { name: 'Real-Time Updates', description: 'Instantly see payment updates.', icon: 'mdi:refresh' },
-        { name: 'Payment App Integration', description: 'Integration with Venmo and other payment platforms.', icon: 'mdi:credit-card' },
+        { name: 'Multiple Groups', description: 'Split expenses among selectable group members.', icon: 'material-symbols:groups' },
+        { name: 'Multi-Currency', description: 'Multi-currency support for international use.', icon: 'bi:currency-exchange' },
+        { name: 'Real-Time Updates', description: 'Instantly see payment updates.', icon: 'tabler:clock-filled' },
+        { name: 'Payment App Integration', description: 'Integration with Venmo and other payment platforms.', icon: 'si:credit-card-detailed-fill' },
       ]
     }
   },
