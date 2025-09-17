84e1ab49b6c1e057ea089278c230f1ea73e1c2f7
samueljkauff <samuelkauff@gmail.com>
2025-09-16 20:55:53 -0400

    co pilot suggestions

--- inertia/pages/home.vue
+++ inertia/pages/home.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class=" mx-[12.5%]">
+  <div class="mx-[12.5%]">
   <p class="font-bold text-4xl w-full text-center mt-8">Features</p>
   <p class="text-center text-xl my-4">Discover the powerful features that make our app stand out.</p>
   <div class="grid grid-cols-3">
     <div v-for="(card, index) in featureCards" :key="index" class="card flex flex-col justify-center items-center p-10 m-4 hover:scale-105 transition-transform duration-300">
-        <Icon class="mx-1 text-7xl m-15 text-accent" :icon="card.icon"/>
+        <Icon class="mx-1 text-7xl m-16 text-accent" :icon="card.icon"/>
         <p class="font-bold text-2xl">{{ card.name }}</p>
         <p class="text-md my-2">{{ card.description }}</p>
     </div>
