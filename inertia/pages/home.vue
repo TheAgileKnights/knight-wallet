c3c3133b9bba581566e262cbf67efe9ccf109968
samueljkauff <samuelkauff@gmail.com>
2025-09-16 20:45:10 -0400

    finished until feedback on styling

--- inertia/pages/home.vue
+++ inertia/pages/home.vue
@@ -1,12 +1,32 @@
 <template>
-Hello World
+  <div class=" mx-[12.5%]">
+  <p class="font-bold text-4xl my-4 w-full text-center">Features</p>
+  <p class="text-center">Discover the powerful features that make our app stand out.</p>
+  <div class="grid grid-cols-3">
+    <div v-for="(card, index) in featureCards" :key="index" class="card flex flex-col justify-center items-center p-10 m-4 hover:scale-105 transition-transform duration-300">
+        <Icon class="mx-1 text-7xl m-15 text-accent" :icon="card.icon"/>
+        <p class="font-bold text-2xl">{{ card.name }}</p>
+        <p class="text-md my-2">{{ card.description }}</p>
+    </div>
+  </div>
+  </div>
 </template>
 
 <script lang="ts">
+
 export default {
   components: {},
   data() {
-    return {}
+    return {
+      featureCards: [
+        { name: 'Group Creation', description: 'Create friend groups and manage members on the go.', icon: 'mdi:people' },
+        { name: 'Organization', description: 'Add and categorize expenses with ease.', icon: 'eos-icons:organization' },
+        { name: 'Multiple Groups', description: 'Split expenses among selectable group members.', icon: 'codicon:organization' },
+        { name: 'Multi-Currency', description: 'Multi-currency support for international use.', icon: 'mdi:currency-usd' },
+        { name: 'Real-Time Updates', description: 'Instantly see payment updates.', icon: 'mdi:refresh' },
+        { name: 'Payment App Integration', description: 'Integration with Venmo and other payment platforms.', icon: 'mdi:credit-card' },
+      ]
+    }
   },
 }
 </script>
