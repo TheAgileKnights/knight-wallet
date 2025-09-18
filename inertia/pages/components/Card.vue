d45cc13551d6dda2cb39cf3a86598104275762af
martipops <melonnemann@gmail.com>
2025-09-18 13:15:11 -0400

    SD-19: Abstract card layout into its own component (#9)

---
+++ inertia/pages/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="bg-background-light shadow-[0_10px_30px_rgba(0,0,0,0.1)] border-[1px] border-border rounded-3xl flex flex-col justify-center items-center p-8 hover:scale-105 transition-transform duration-300 text-center gap-2"
+  >
+    <div class="my-4">
+      <slot name="icon"/>
+    </div>
+    <div class="font-bold text-2xl">
+      <slot name="title"/>
+    </div>
+    <div class="text-lg text-secondary-text">
+      <slot name="description"/>
+    </div>
+  </div>
+</template>
