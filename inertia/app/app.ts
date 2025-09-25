a667c18bd2d8007b64cbc7e11e9ff9658190473e
martipops <melonnemann@gmail.com>
2025-09-24 22:47:26 -0400

    Change app name

--- inertia/app/app.ts
+++ inertia/app/app.ts
@@ -8,12 +8,12 @@ import { createInertiaApp } from '@inertiajs/vue3'
 import { resolvePageComponent } from '@adonisjs/inertia/helpers'
 import { Icon } from '@iconify/vue'
 
-const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
+const appName = import.meta.env.VITE_APP_NAME || 'Knight Wallet'
 
 createInertiaApp({
   progress: { color: '#5468FF' },
 
-  title: (title) => `${title} - ${appName}`,
+  title: () => appName,
 
   resolve: (name) => {
     return resolvePageComponent(
