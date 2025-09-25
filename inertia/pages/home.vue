10da49ec648cb4c43831bafd985ee61ad8745ada
martipops <melonnemann@gmail.com>
2025-09-24 22:31:47 -0400

    Code smell

--- inertia/pages/home.vue
+++ inertia/pages/home.vue
@@ -100,7 +100,6 @@
           mozAllowFullScreen
           allow="autoplay *; fullscreen *; encrypted-media *"
           sandbox="allow-downloads allow-forms allow-same-origin allow-scripts allow-top-navigation allow-pointer-lock allow-popups allow-modals allow-orientation-lock allow-popups-to-escape-sandbox allow-presentation allow-top-navigation-by-user-activation"
-          frameborder="0"
           title="Knight Wallet"
         ></iframe>
       </div>
