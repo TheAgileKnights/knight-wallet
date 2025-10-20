67a0f5b5efb943d08de67f41f65070447ca595b3
samueljkauff <samuelkauff@gmail.com>
2025-10-19 21:50:04 -0400

    added interviews page

--- inertia/pages/designArtifacts.vue
+++ inertia/pages/designArtifacts.vue
@@ -1,11 +1,37 @@
 <template>
-Design Artifacts
+  <p class="text-3xl">Design Artifacts</p>
+  <div>
+  <interviews :interviews="jsonData.interviews" :name="jsonData.name"/>
+  </div>
 </template>
 
 <script lang="ts">
-
+import Interviews from './components/interviews.vue'
 export default {
-
+  components: {
+    Interviews
+  },
+  data() {
+    return {
+        jsonData: {
+            name: "Jimmy Dean",
+            interviews: [
+                {
+                    "question": "Question 1",
+                    "answer": "BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH"
+                },
+                {
+                    "question": "Question 2",
+                    "answer": "BLAH BLAH BLAHBLAH BLAH BLAH"
+                },
+                {
+                    "question": "Question 3",
+                    "answer": "BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH"
+                },
+            ]
+        },
+    }
+  },
 }
 
 </script>
