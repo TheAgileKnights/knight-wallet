b45cb71f9efe7a3071d3a4f895f7bbacbedec798
martipops <melonnemann@gmail.com>
2025-10-20 16:56:20 -0400

    Squashed commit of the following:
    commit c9034dbb08a562ae11c93eb0efb5e702ccc5383a
    Author: martipops <melonnemann@gmail.com>
    Date:   Mon Oct 20 16:54:51 2025 -0400
    
        Added scenarios and fixed responsiveness

---
+++ inertia/pages/components/Scenarios.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
+    <Card class="col-span-full" :hover-effect="false">
+      <p class="text-3xl font-bold">Scenarios</p>
+    </Card>
+    <Card v-for="(scenario, index) in scenarios" :key="index" class="">
+      <template #icon>
+        <icon
+          :icon="`material-symbols:counter-${index + 1}-rounded`"
+          class="text-accent w-16 h-16"
+        ></icon>
+      </template>
+      <template #title>
+        <p>{{ scenario.title }}</p>
+      </template>
+      <template #description>
+        <div class="text-left space-y-4 text-text-secondary">
+          <p>{{ scenario.description }}</p>
+        </div>
+      </template>
+    </Card>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import Card from './Card.vue'
+import { Scenario } from '../designArtifacts.vue'
+
+export default defineComponent({
+  name: 'Scenarios',
+  components: {
+    Card,
+  },
+  props: {
+    scenarios: {
+      type: Array as PropType<Scenario[]>,
+      required: true,
+    },
+  },
+})
+</script>
