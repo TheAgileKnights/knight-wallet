a2d55ec87dfd33cf0ed2c153dd240fe4b4f40232
martipops <melonnemann@gmail.com>
2025-10-20 16:35:46 -0400

    Added features and updated animations

---
+++ inertia/pages/components/Features.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+    <Card v-for="(feature, index) in features" :key="index" :hover-effect="false">
+      <template #icon>
+        <icon :icon="feature.icon" class="text-accent w-32 h-32"/>
+      </template>
+      <template #title>
+        <p>{{ feature.title }}</p>
+      </template>
+      <template #description>
+        <div class="text-left space-y-4 text-text-secondary">
+          <div class="my-2">
+            <strong>Description:</strong>
+          </div>
+          <ul class="list-disc list-outside pl-5 space-y-2">
+            <li
+              v-for="(desc, dIndex) in feature.description"
+              :key="dIndex"
+              class="text-text-secondary leading-relaxed"
+            >
+              {{ desc }}
+            </li>
+          </ul>
+          <div class="space-y-2 text-text-secondary">
+            <p><strong>Constraints:</strong> {{ feature.constraints }}</p>
+            <p><strong>Comments:</strong> {{ feature.comments }}</p>
+          </div>
+        </div>
+      </template>
+    </Card>
+  </div>
+</template>
+
+<script lang="ts">
+export type Feature = {
+  title: string
+  icon?: string
+  description: string[]
+  constraints: string
+  comments: string
+}
+
+import { PropType } from 'vue'
+import Card from './Card.vue'
+
+export default {
+  name: 'Features',
+  components: {
+    Card,
+  },
+  props: {
+    features: {
+      type: Array as PropType<Feature[]>,
+      required: true,
+    },
+  },
+}
+</script>
