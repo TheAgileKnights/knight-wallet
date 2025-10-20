d4e509bd166f2115eb288dc2498def3b95716786
samueljkauff <samuelkauff@gmail.com>
2025-10-19 23:17:38 -0400

    Changed styling

--- inertia/pages/components/Personas.vue
+++ inertia/pages/components/Personas.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="bg-background-light box w-full flex flex-col items-center mb-8">
-    <h1 class="text-3xl font-bold mt-4 text-text flex">Persona: {{ name }}</h1>
-    <h1 class="text-3xl font-bold mb-4 text-text flex">Age: {{ age }}</h1>
-  </div>
-<!-- <div class="box border-[1px] border-border rounded-3xl flex justify-center items-start flex-col p-8 max-w-5xl mx-auto bg-background-light"> -->
-  <Card>
-    <p class="text-2xl whitespace-pre-line text-text-secondary">{{ persona }}</p>
+  <Card class="m-10">
+    <template #icon>
+      <icon icon="material-symbols:account-circle" class="text-primary w-32 h-32"></icon>
+    </template>
+    <template #title>
+      <p>{{ `${name}, ${age} years old` }}</p>
+    </template>
+    <template #description>
+      <p class="text-2xl whitespace-pre-line text-text-secondary">{{ persona }}</p>
+    </template>
   </Card>
 <!-- </div> -->
 </template>
