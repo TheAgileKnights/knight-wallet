1036d6c7969e9daf13e28408ea8bc61ab7f397a2
martipops <melonnemann@gmail.com>
2025-10-22 17:48:16 -0400

    Dont constrain w and h

--- inertia/pages/components/Dialog.vue
+++ inertia/pages/components/Dialog.vue
@@ -6,11 +6,11 @@
     class="bg-[rgba(0,0,0,0.4)] w-full h-full fixed top-0 left-0 z-10"
   >
     <dialog
-      class="z-20 p-8 min-w-1/3 min-h-1/2 max-w-screen max-h-screen rounded-3xl fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 m-0 drop-shadow-[0_10px_30px_rgba(0,0,0,0.6)]"
+      class="z-20 p-8 max-w-screen max-h-screen rounded-3xl fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 m-0 drop-shadow-[0_10px_30px_rgba(0,0,0,0.6)]"
       open
     >
     <div class="flex flex-col">
-      <div class="flex justify-end items-center">
+      <div class="flex justify-end items-center gap-4">
         <div class="grow">
           <slot name="header">
             <h3 class="text-xl font-bold">
