1c4441550b77be9134661c2e711745ed63ea44be
samueljkauff <samuelkauff@gmail.com>
2025-10-22 15:35:23 -0400

    fixed height and width

--- inertia/pages/components/Dialog.vue
+++ inertia/pages/components/Dialog.vue
@@ -1,10 +1,7 @@
 <template>
-    <dialog :class="{ hidden: !modelValue }" ref="dialog" class="bg-background-light border border-border rounded-3xl fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 m-0 drop-shadow-[0_10px_30px_rgba(0,0,0,1)]" open>
+    <dialog :class="{ hidden: !modelValue }" ref="dialog" class="min-w-1/3 min-h-1/2 bg-background-light border border-border rounded-3xl fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 m-0 drop-shadow-[0_10px_30px_rgba(0,0,0,1)]" open>
       <form method="dialog">
-        <input class="block"></input>
-        <input class="block"></input>
-        <input class="block"></input>
-        <button class="block" type="button" @click="closeDialog()">CLOSE</button>
+        <button class="flex justify-end border border-accent" type="button" @click="closeDialog()">CLOSE</button>
       </form>
     </dialog>
 </template>
