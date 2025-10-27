40febd32a28e65b0d588e3a46b434f89c45be73d
martipops <melonnemann@gmail.com>
2025-10-27 15:02:49 -0400

    Implemented fakeuser functionality

--- app/services/user_service.ts
+++ app/services/user_service.ts
@@ -1,9 +1,10 @@
 import User from '#models/user'
 import { createUserValidator } from '#validators/user'
 import { GoogleUser } from '../types/user.js'
+import type { HttpContext } from '@adonisjs/core/http'
 
 export class UserService {
-  static async findOrCreateUser(googleUser: GoogleUser) {
+  static async findOrCreateUserFromGoogle(googleUser: GoogleUser) {
     const user = await createUserValidator.validate(googleUser)
     return User.firstOrCreate(
       { googleId: user.id },
@@ -14,5 +15,23 @@ export class UserService {
         username: user.email.split('@')[0],
       }
     )
+  }
+  static async loginUser(user: User, ctx: HttpContext) {
+    await ctx.auth.use('web').login(user)
+    ctx.session.flash('success', 'You are now logged in.')
+    return ctx.response.redirect().toRoute('dashboard')
+  }
+  static async registerAndLoginGoogleUser(googleUser: GoogleUser, ctx: HttpContext) {
+    const user = await this.findOrCreateUserFromGoogle(googleUser)
+    return this.loginUser(user, ctx)
+  }
+  static async registerAndLoginFakeUser(ctx: HttpContext) {
+    const fakeUser: GoogleUser = {
+      id: 'fake-google-id',
+      name: 'Fake User',
+      email: 'fakeuser@example.com',
+      avatarUrl: 'https://www.w3schools.com/howto/img_avatar.png',
+    }
+    return this.registerAndLoginGoogleUser(fakeUser, ctx)
   }
 }
