6b2445e83e565438a82baf817d3c171474ccc346
martipops <melonnemann@gmail.com>
2025-10-30 14:38:55 -0400

    Formatting

--- inertia/pages/components/Input.vue
+++ inertia/pages/components/Input.vue
@@ -1,26 +1,30 @@
 <template>
-        <small v-if="label" class="text-text">{{ label }}</small>
-        <input :type="type" :placeholder="placeholder" class="block p-2 text-text border-border border-2 rounded-xl focus:outline-2 focus:outline-accent placeholder:text-text-secondary"></input>
+  <small v-if="label" class="text-text">{{ label }}</small>
+  <input
+    :type="type"
+    :placeholder="placeholder"
+    class="block p-2 text-text border-border border-2 rounded-xl focus:outline-2 focus:outline-accent placeholder:text-text-secondary"
+  />
 </template>
 
 <script lang="ts">
-import { PropType } from 'vue';
-import type { InputTypeHTMLAttribute } from 'vue';
+import { PropType } from 'vue'
+import type { InputTypeHTMLAttribute } from 'vue'
 export default {
-    props: {
-        type: {
-            type: String as PropType<InputTypeHTMLAttribute>,
-            required: false,
-            default: 'text',
-        },
-        label: {
-            type: String,
-            required: false,
-        },
-        placeholder: {
-            type: String,
-            required: false,
-        }
-    }
+  props: {
+    type: {
+      type: String as PropType<InputTypeHTMLAttribute>,
+      required: false,
+      default: 'text',
+    },
+    label: {
+      type: String,
+      required: false,
+    },
+    placeholder: {
+      type: String,
+      required: false,
+    },
+  },
 }
 </script>
