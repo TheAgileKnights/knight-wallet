2900bcc63d6c08f1bb53ef07102e1725b1450299
martipops <melonnemann@gmail.com>
2025-11-19 15:08:11 -0500

    Initial selectchips

---
+++ inertia/pages/components/SelectChips.vue
@@ -0,0 +1,32 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { PropType } from 'vue'
+
+export interface SelectChipsOption {
+  label: string
+  value: any
+}
+
+export type SelectChipsValue<T> = Array<T> | T
+
+export default {
+  name: 'SelectChips',
+  props: {
+    options: {
+      type: Object as PropType<SelectChipsOption[]>,
+      required: true,
+    },
+    modelValue: {
+      type: Object as PropType<SelectChipsValue<any>>,
+      required: true,
+    },
+    single: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
