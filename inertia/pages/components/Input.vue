20a75b11811bc0866289a591a1911acbc6b2769b
Sam Kauffman <109776716+Samueljkauff@users.noreply.github.com>
2025-11-19 15:00:49 -0500

    Sd 42: Custom Input (#19)
    Co-authored-by: martipops <melonnemann@gmail.com>

---
+++ inertia/pages/components/Input.vue
@@ -0,0 +1,30 @@
+<template>
+  <small v-if="label" class="text-text">{{ label }}</small>
+  <input
+    :type="type"
+    :placeholder="placeholder"
+    class="block p-2 text-text-secondary border-border focus:border-accent border-2 rounded-xl outline-0 placeholder:text-shadow-text-secondary"
+  />
+</template>
+
+<script lang="ts">
+import { PropType } from 'vue'
+import type { InputTypeHTMLAttribute } from 'vue'
+export default {
+  props: {
+    type: {
+      type: String as PropType<InputTypeHTMLAttribute>,
+      required: false,
+      default: 'text',
+    },
+    label: {
+      type: String,
+      required: false,
+    },
+    placeholder: {
+      type: String,
+      required: false,
+    },
+  },
+}
+</script>
