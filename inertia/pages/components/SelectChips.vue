d88e4a2915e6e983f2ecd1ef519079f04c740f50
martipops <melonnemann@gmail.com>
2025-11-19 15:53:21 -0500

    SD-27: Create SelectChips.vue (#21)

---
+++ inertia/pages/components/SelectChips.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="flex gap-1">
+    <template v-for="option in options" :key="option.value">
+      <div
+        class="cursor-pointer rounded-xl px-4 py-2 border-2 border-border"
+        :class="{
+          'bg-accent text-white border-accent-hover!': single
+            ? modelValue === option.value
+            : modelValue.includes(option.value),
+          'bg-background-light text-text-secondary': single
+            ? modelValue !== option.value
+            : !modelValue.includes(option.value),
+        }"
+        @click="selectOption(option)"
+      >
+        {{ option.label }}
+      </div>
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+import { PropType } from 'vue'
+
+export interface SelectChipsOption {
+  label: string
+  value: any
+}
+
+export type SelectChipsValue<T> = Array<T> | T
+
+export default {
+  name: 'SelectChips',
+  props: {
+    options: {
+      type: Object as PropType<SelectChipsOption[]>,
+      required: true,
+    },
+    modelValue: {
+      type: [String, Number, Array] as PropType<SelectChipsValue<any>>,
+      required: true,
+    },
+    single: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ['update:modelValue'],
+  computed: {
+    selection: {
+      get(): SelectChipsValue<any> {
+        return this.modelValue
+      },
+      set(value: SelectChipsValue<any>) {
+        this.$emit('update:modelValue', value)
+      },
+    },
+  },
+  methods: {
+    selectOption(option: SelectChipsOption) {
+      if (this.single) {
+        this.selection = option.value
+      } else {
+        const currentSelection = this.selection as Array<any>
+        if (currentSelection.includes(option.value)) {
+          this.selection = currentSelection.filter((val) => val !== option.value)
+        } else {
+          this.selection = [...currentSelection, option.value]
+        }
+      }
+    },
+  },
+}
+</script>
