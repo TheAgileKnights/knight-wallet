26a9adcc5fc856b8c6717cb9ae6a2775b3db4b4b
martipops <melonnemann@gmail.com>
2025-11-28 14:54:47 -0500

    Squashed commit of the following:
    commit 75ef02eb9bba56946a946b3eb8bf410e055c5d43
    Author: martipops <melonnemann@gmail.com>
    Date:   Fri Nov 28 14:54:20 2025 -0500
    
        Fixed login UX issue
    
    commit e0c017d47fc820fc57ec8ad53363e0a990b90e7a
    Author: martipops <melonnemann@gmail.com>
    Date:   Fri Nov 28 14:52:09 2025 -0500
    
        Fixed icons reusability

--- database/seeders/icon_seeder.ts
+++ database/seeders/icon_seeder.ts
@@ -1,17 +1,9 @@
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
 import db from '@adonisjs/lucid/services/db'
+import { ICON_DEFINITIONS } from '#constants/icons'
 
 export default class extends BaseSeeder {
   async run() {
-    await db.table('icons').multiInsert([
-      { icon_string: 'majesticons:money-line', name: 'Bill' },
-      { icon_string: 'majesticons:burger-line', name: 'Food' },
-      { icon_string: 'majesticons:car-line', name: 'Transportation' },
-      { icon_string: 'icon-park-outline:popcorn', name: 'Entertainment' },
-      { icon_string: 'majesticons:shopping-cart-line', name: 'Shopping' },
-      { icon_string: 'majesticons:pulse', name: 'Health' },
-      { icon_string: 'majesticons:lightbulb-shine-line', name: 'Utilities' },
-      { icon_string: 'majesticons:more-menu', name: 'Other' },
-    ])
+    await db.table('icons').multiInsert(ICON_DEFINITIONS)
   }
 }
